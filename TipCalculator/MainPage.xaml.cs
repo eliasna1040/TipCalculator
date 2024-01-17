70dd54f052b430902c3db4e8087891c817c58095
1micr01 <elias11a.e@gmail.com>
2024-01-17 21:51:04 +0100

    done

--- TipCalculator/MainPage.xaml.cs
+++ TipCalculator/MainPage.xaml.cs
@@ -1,13 +1,23 @@
-﻿namespace TipCalculator;
+﻿using System.Globalization;
+
+namespace TipCalculator;
 
 public partial class MainPage : ContentPage
 {
+    private CultureInfo _currencyCulture;
+    
     public MainPage()
     {
         InitializeComponent();
+        _currencyCulture = CultureInfo.GetCultureInfo("da-DK");
         TipSlider.Value = 15;
     }
 
+    #region EventHandlers
+
+    
+
+    
     private void AmountEntry_OnTextChanged(object? sender, TextChangedEventArgs e)
     {
         Calculate(false, false);
@@ -29,35 +39,70 @@ public partial class MainPage : ContentPage
         Calculate(false, true);
     }
 
-    private void TwentyPercentageButton_OnClicked(object? sender, EventArgs e)
+    private async void TwentyPercentageButton_OnClicked(object? sender, EventArgs e)
     {
-        TipSlider.Value = 20;
+        SetTip(20);
     }
 
-    private void FifteenPercentageButton_OnClicked(object? sender, EventArgs e)
+    private async void FifteenPercentageButton_OnClicked(object? sender, EventArgs e)
     {
-        TipSlider.Value = 15;
+        SetTip(15);
+    }
+    
+    private async void CurrencyButton_OnClicked(object? sender, EventArgs e)
+    {
+        string currency = await DisplayActionSheet("Currency", "Cancel", null, "DKK", "EUR", "USD");
+        string type = currency switch
+        {
+            "DKK" => "da-DK",
+            "EUR" => "de-DE",
+            "USD" => "en-US",
+            _ => "da-DK"
+        } ;
+
+        _currencyCulture = CultureInfo.GetCultureInfo(type);
+        Calculate(false, false);
     }
 
+    #endregion
+
+    #region Logic
+
+    
+
+    private async void SetTip(double sliderValue)
+    {
+        double tipValue = double.TryParse(AmountEntry.Text, out double amount) ? CalculateTip(amount, sliderValue) : 0;
+        
+        if (await DisplayAlert("Tip", $"Do you want to pay a {tipValue.ToString("C", _currencyCulture)} tip", "Yes", "No"))
+        {
+            TipSlider.Value = sliderValue;
+        }
+    }
+
+    private double CalculateTip(double amount, double tip)
+    {
+        return Math.Round(tip, MidpointRounding.ToEven) / 100 * amount;
+    }
+    
     private void Calculate(bool roundUp, bool roundDown)
     {
-        if (double.TryParse(AmountEntry.Text, out double amount))
+        double.TryParse(AmountEntry.Text, out double amount);
+
+        if (roundUp)
         {
-            if (roundUp)
-            {
-                amount = Math.Ceiling(amount / 10) * 10;
-            }
-            else if (roundDown)
-            {
-                amount = Math.Floor(amount / 10) * 10;
-            }
-            
-            double tip = Math.Round(TipSlider.Value, MidpointRounding.ToEven) / 100 * amount;
-            double totalAmount = amount > 0 ? amount + tip : 0;
-            
-            TipLabel.Text = tip.ToString("C");
-            TotalLabel.Text = totalAmount.ToString("C");
+            amount = Math.Ceiling(amount / 10) * 10;
         }
-        
+        else if (roundDown)
+        {
+            amount = Math.Floor(amount / 10) * 10;
+        }
+            
+        double tip = CalculateTip(TipSlider.Value, amount);
+        double totalAmount = amount > 0 ? amount + tip : 0;
+            
+        TipLabel.Text = tip.ToString("C", _currencyCulture);
+        TotalLabel.Text = totalAmount.ToString("C", _currencyCulture);
     }
+    #endregion
 }

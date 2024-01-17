cc48f16e7880d2b2d17f3ebd727e5429d586a8a8
c98101ena <elias.andersen@hessel.dk>
2024-01-17 10:58:38 +0100

    navigation done

---
+++ TipCalculator/FeedbackPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipCalculator;
+
+public partial class FeedbackPage : ContentPage
+{
+    public FeedbackPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void GoBackButton_OnClicked(object? sender, EventArgs e)
+    {
+        await Navigation.PopModalAsync();
+    }
+}

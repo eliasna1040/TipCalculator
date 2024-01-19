b3f8cc715690bb4e1b945b5875827c55ece2821b
1micr01 <elias11a.e@gmail.com>
2024-01-19 08:30:57 +0100

    add and delete don

---
+++ TipCalculator/Models/Restaurant.cs
@@ -0,0 +1,8 @@
+namespace TipCalculator.Models;
+
+public class Restaurant
+{
+    public string Name { get; set; } = "";
+    public string ImageUrl { get; set; } = "";
+    public int TipPercentage { get; set; }
+}
